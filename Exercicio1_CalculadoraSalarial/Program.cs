03ea63c58f4e9162c9ac8249067329fcb3d5701e
João Marcos <joaomarcosmiranda658@gmail.com>
2026-06-01 20:21:46 -0300

    Classe program.cs adicionada com interação ao usuario

--- Exercicio1_CalculadoraSalarial/Program.cs
+++ Exercicio1_CalculadoraSalarial/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Exercicio1_CalculadoraSalarial.Models;
+using System;
 
 namespace Exercicio1_CalculadoraSalarial
 {
@@ -6,7 +7,47 @@ namespace Exercicio1_CalculadoraSalarial
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("===== CALCULADORA SALARIAL =====");
+            Console.WriteLine("===== CALCULADORA SALARIAL =====\n");
+
+
+            while (true)
+            {
+                try
+                {
+                    Console.Write("\nDigite o valor da hora trabalhada: ");
+                    decimal valorSalarioHora = decimal.Parse(Console.ReadLine());
+
+                    Console.Write("\nDigite a quantidade de horas trabalhadas: ");
+                    decimal horasTrabalhadasMensal = decimal.Parse(Console.ReadLine());
+
+                    Console.Write("\nDigite a quantidade de filhos menores de 14 anos: ");
+                    int filhosMenoresQuatorze = int.Parse(Console.ReadLine());
+
+                    Empregado empregado = new Empregado(valorSalarioHora, horasTrabalhadasMensal, filhosMenoresQuatorze);
+
+                    Console.WriteLine("\n--- RESULTADO ---");
+                    Console.WriteLine("O SALÁRIO BRUTO É: " + empregado.CalcularSalarioBruto().ToString("C"));
+                    Console.WriteLine("O SALÁRIO FAMÍLIA É: " + empregado.CalcularSalarioFamilia().ToString("C"));
+                    Console.WriteLine("O SALÁRIO LÍQUIDO É: " + empregado.CalcularSalarioLiquido().ToString("C"));
+                    Console.WriteLine("-----------------\n");
+
+                    Console.WriteLine("Deseja calcular o salário de outro empregado? (S/N)");
+                    string escolha = Console.ReadLine().ToUpper();
+
+                    if(escolha != "S")
+                    {
+                        Console.WriteLine("===APLICAÇÃO FINALIZADA====");
+                        break;
+                    }
+
+                  
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("\n[Erro] Formato inválido. Por favor, digite apenas números.");
+                }
+            }
+            
 
         }
     }
