9a59e6ed47f51c562ef3cd960db661b7287a85cd
João Marcos <joaomarcosmiranda658@gmail.com>
2026-06-02 01:42:25 -0300

    logica de fibonacci adicionada

--- Exercicio3_Fibonacci/Program.cs
+++ Exercicio3_Fibonacci/Program.cs
@@ -8,5 +8,25 @@ class Program
     {
         Console.WriteLine("===== LÓGICA DE FIBONACCI =====");
 
+        Console.WriteLine("Digite quantos numeros você deseja ver da sequência de Fibonacci:");
+        int qtdNumero = int.Parse(Console.ReadLine());
+
+        int valorAnterior = 0;
+        int valorAtual = 1;
+        int soma = valorAnterior + valorAtual;
+
+        Console.WriteLine("A sequencia é: ");
+
+        for(int i = 0; i < qtdNumero; i++)
+        {
+            Console.WriteLine($" {soma}");
+
+            valorAnterior = valorAtual;
+            valorAtual = soma;
+            soma = valorAnterior + valorAtual;
+
+        }
+
+
     }
 }
