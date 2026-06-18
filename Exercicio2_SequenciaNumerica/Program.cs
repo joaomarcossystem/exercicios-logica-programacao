6ce2e908374c20b3947e10b279888723dbbc5d2f
João Marcos <joaomarcosmiranda658@gmail.com>
2026-06-17 21:11:30 -0300

    Segundo maior número adicionado

--- Exercicio2_SequenciaNumerica/Program.cs
+++ Exercicio2_SequenciaNumerica/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace Exercicio2_SequenciaNumerica;
 
@@ -7,7 +8,7 @@ class Program
     public static void Main(string[] args)
     {
         Console.WriteLine("===== SEQUÊNCIA NUMÉRICA =====");
-
+        
         try
         {
             Console.WriteLine("\nDigite a quantidade de números que sua sequência possuirá.");
@@ -25,6 +26,7 @@ class Program
 
             Console.WriteLine("\n ------RESULTADO------\n");
             Console.WriteLine("Sequência digitada: ");
+
             for(int i = 0; i < sequencia.Length; i++)
             {
                 Console.Write(sequencia[i] + "; ");
@@ -34,28 +36,40 @@ class Program
 
             int menor = sequencia[0];
             int maior = sequencia[0]; //assumir que o primeiro elemento é o menor e o maior valor
+            int segundoMaior = int.MinValue;
 
-            for(int i = 0; i < sequencia.Length; i++)
+
+            for (int i = 0; i < sequencia.Length; i++)
             {
+                // Lógica do Menor
                 if (sequencia[i] < menor)
                 {
                     menor = sequencia[i];
                 }
+
+                // Lógica do Maior e Segundo Maior
                 if (sequencia[i] > maior)
                 {
+                    // O antigo maior cai para segundo lugar antes da atualização
+                    segundoMaior = maior;
                     maior = sequencia[i];
+                }
+                else if (sequencia[i] > segundoMaior && sequencia[i] != maior)
+                {
+                    // Atualiza o segundo maior, garantindo que não é o mesmo valor do maior
+                    segundoMaior = sequencia[i];
                 }
             }
 
             Console.WriteLine($"O maior número é: {maior}");
             Console.WriteLine($"O menor número é: {menor}");
+            Console.WriteLine($"O segundo maior é: {segundoMaior}");
 
             Console.WriteLine();
             Console.WriteLine("===== APLICAÇÃO FINALIZADA =====");
 
-
-
         }
+
         catch (FormatException)
         {
             Console.WriteLine("\n [ERRO] Digite números válidos.");
@@ -64,3 +78,17 @@ class Program
     }
 
 }
+
+
+            /*
+            if (sequencia[i] < segundoMaior )
+            {
+                segundoMaior = sequencia[i] - maior;  >>>> tentativas feitas na entrevista
+            }
+
+            /*
+            if (segundoMaior < maior)
+            {
+                segundoMaior  = sequencia[i]; >>>> tentativas feitas na na entrevista
+            }
+            */
